2cc5c2a8aa63e72dad99ee17f12ef400eb14ef5b
lucaalessandrelli <10533904@polimi.it>
2018-05-16 17:49:17 +0200

    Modified test

--- src/test/java/it/polimi/ingsw/TurnTest.java
+++ src/test/java/it/polimi/ingsw/TurnTest.java
@@ -1,19 +1,11 @@
 package it.polimi.ingsw;
 
-import it.polimi.ingsw.model.gameData.Player;
-import it.polimi.ingsw.model.gameData.ToolCard;
-import it.polimi.ingsw.model.gameLogic.Move;
-import it.polimi.ingsw.turn.Turn;
 import org.junit.jupiter.api.Test;
 
 public class TurnTest {
     @Test
     public void instanceTest() {
-        Move move = new ToolCard();
-        Player player = new Player("1");
 
-        Turn turn = new Turn(player);
-        turn.startTurn();
 
     }
 }
