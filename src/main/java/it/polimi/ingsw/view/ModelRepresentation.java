9136eff7994e5d5eb48106f2dd23a9f4257c336e
giauen <37808383+giauen@users.noreply.github.com>
2018-05-28 22:57:57 +0200

    view for client

---
+++ src/main/java/it/polimi/ingsw/view/ModelRepresentation.java
@@ -0,0 +1,11 @@
+package it.polimi.ingsw.view;
+
+public class ModelRepresentation {
+    String windows;
+    String draftPool;
+    String serviceMessage;
+
+    public void setService(String serviceMessage) {
+        this.serviceMessage=serviceMessage;
+    }
+}
