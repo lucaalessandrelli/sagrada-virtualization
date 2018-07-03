fa3aa783ef99b32d3db90eb7e20fa8f9957dfc4e
lucaalessandrelli <10533904@polimi.it>
2018-07-03 12:56:51 +0200

    Added javadoc

--- src/main/java/it/polimi/ingsw/turn/ChooseDice2.java
+++ src/main/java/it/polimi/ingsw/turn/ChooseDice2.java
@@ -5,6 +5,11 @@ import it.polimi.ingsw.model.gamedata.gametools.Dice;
 import it.polimi.ingsw.model.gamelogic.checker.InspectorPlace;
 import it.polimi.ingsw.turn.moveexceptions.WrongMoveException;
 
+/**
+ * Class defining the concrete state ChooseDice2,in this state a player can chose a Position, or Pass move.
+ * If he choose a Position the next state would be "EndTurn". Finally if he choose to pass
+ * the next state would also be "EndTurn".
+ */
 public class ChooseDice2 implements TurnState {
     private Turn turn;
     private Pos posDiceChosen;
@@ -13,6 +18,15 @@ public class ChooseDice2 implements TurnState {
     private Pos toolPos;
     private InspectorPlace inspectorPlace;
 
+    /**
+     * Classic constructor.
+     * @param turn Store the Turn object in order to call methods on it, like changing the concrete state of the turn calling setState().
+     * @param chosenDice The Dice the player has just selected.
+     * @param posDiceChosen The Position of the Dice the player has just selected (chosenDice).
+     * @param toolDice The dice has chosen through the toolCard.
+     * @param toolPos The position of the the Dice the player has chosen through the toolCard (toolDice).
+     *
+     */
     public ChooseDice2(Turn turn, Dice chosenDice,Pos posDiceChosen, Dice toolDice, Pos toolPos) {
         this.turn = turn;
         this.chosenDice = chosenDice;
@@ -24,6 +38,11 @@ public class ChooseDice2 implements TurnState {
 
     //GETTING MOVE METHODS
 
+    /**
+     * {@inheritDoc}
+     * In this implementation the method check() on the inspectorPlace is being called to actually understand whether the move is
+     * valid or not. If it is valid then we change state, we throw the exception otherwise.
+     */
     @Override
     public void receiveMove(Pos pos) throws WrongMoveException{
         if(inspectorPlace.check(chosenDice,pos,turn.getPlayer().getWindowPatternCard())) {
