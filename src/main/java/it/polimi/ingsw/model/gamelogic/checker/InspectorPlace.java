e3f79d2f15ca6b2a3cb954bb6cda214ce44f60e1
giauen <37808383+giauen@users.noreply.github.com>
2018-07-07 15:11:38 +0200

    fix

--- src/main/java/it/polimi/ingsw/model/gamelogic/checker/InspectorPlace.java
+++ src/main/java/it/polimi/ingsw/model/gamelogic/checker/InspectorPlace.java
@@ -181,7 +181,8 @@ public class InspectorPlace {
         int y = pos.getY();
         for (int i = -1; i<=1; i++){
             for(int j =1 ; j>= -1; j--){
-                if(x+i>=MINROW && x+i<=MAXROW && y+j>=MINCOL && y+j<=MAXCOL && window.getCell(new Pos(x+i,y+j)).isOccupied()){
+                if(x+i>=MINROW && x+i<=MAXROW && y+j>=MINCOL && y+j<=MAXCOL && window.getCell(new Pos(x+i,y+j)).isOccupied()&&
+                        !window.getDice(new Pos(x+i,y+j)).isSelected()){
                     return false;
                 }
             }
